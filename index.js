9ac1100a378de10f3b346e89b5a62f53b569efe2
rusminanto <tyas@shipper.id>
2021-09-26 22:23:18 +0700

    adding testing for javascript

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function logout() {
-	
+const { Sequelize, QueryTypes } = require('sequelize')
+
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/trolly')
+
+let UniqueNumber = {
+    nextval: ""
 }
+
+const getUniqueSequenceNumber = async (seq = Sequelize) =>  {
+    try {
+        sequelize.authenticate().then(res => {
+            console.log('connection success')
+        })
+        console.log("connection established!")
+        let uQ = UniqueNumber
+        uQ = await sequelize.query(`select nextval('id_inb_order_seq')`, { type: QueryTypes.SELECT })
+    } catch (e) {
+        console.log("error - " + e)
+    }
+}
+
+getUniqueSequenceNumber()
